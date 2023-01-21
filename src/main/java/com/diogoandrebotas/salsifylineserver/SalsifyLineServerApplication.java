a228a390d05b4adb8bdc4c32b10248525a5dbb63
DiogoAndreBotas <gamebreak24@gmail.com>
2023-01-21 15:14:27 +0000

    Add barebones Java + Spring + Gradle application

---
+++ src/main/java/com/diogoandrebotas/salsifylineserver/SalsifyLineServerApplication.java
@@ -0,0 +1,13 @@
+package com.diogoandrebotas.salsifylineserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsifyLineServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalsifyLineServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diogoandrebotas/salsifylineserver/SalsifyLineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogoandrebotas.salsifylineserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsifyLineServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
